49746f006763d33313717ead6007401f058e1df3
scwebd <scwebd@gmail.com>
2019-05-09 12:46:25 -0600

    hmm

---
+++ README.md
@@ -0,0 +1,16 @@
+# React Clicky Game!
+A project I completed during the University Of Denver Full Stack Coding Bootcamp. A simple React game. Where you try to beat your high score by remebering the images as they shuffle.
+## Getting Started
+Follow to link below to play the game!
+
+* [Clicky Game](https://toddtatedev.github.io/clicky-game/) - Clicky Game
+
+
+## Built With
+* [React](https://reactjs.org/) - React
+* [Javascript](https://www.javascript.com/) - Javascript
+* [HTML](https://html.com/) - HTML
+* [CSS](https://www.w3schools.com/css/) - CSS
+## Author
+* **Todd Tate** - *Initial work* - [ToddTateDev](https://github.com/ToddTateDev)
+
---
+++ src/components/Game/index.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import Nav from "../Nav";
+import Header from "../Header";
+import Container from "../Container";
+import ClickItem from "../ClickItem";
+import Footer from "../Footer";
+import characters from "../../characters.json";
+
+class Game extends Component {
+    state = {
+        characters,
+        score: 0,
+        topScore: 0
+    };
+
+    componentDidMount() {
+        this.setState({ characters: this.shuffleData(this.state.characters)});
+    }
+
+    handleCorrectGuess = newData => {
+        const { topScore, score } = this.state;
+        const newScore = score + 1;
+        const newTopScore = Math.max(newScore, topScore);
+
+        this.setState({
+            characters: this.shuffleData(newData),
+            score: newScore,
+            topScore: newTopScore
+        });
+    };
+
+    handleIncorrectGuess =  data => {
+        this.setState({
+            characters: this.resetData(data),
+            score: 0
+        });
+    };
+
+    resetData = characters => {
+        const resetData = characters.map(item => ({ ...item, clicked: false }));
+        return this.shuffleData(resetData);
+    };
+
+    shuffleData = characters => {
+        let i = characters.length - 1;
+        while (i > 0) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = characters[i];
+            characters[i] = characters[j];
+            characters[j] = temp;
+            i--;
+        }
+        return characters;
+    }
+
+    handleItemClick = id => {
+        let guessedCorrectly = false;
+        const newData = this.state.characters.map(item => {
+            const newItem = { ...item };
+            if (newItem.id === id) {
+                if(!newItem.clicked) {
+                    newItem.clicked = true;
+                    guessedCorrectly = true;
+                }
+            }
+            return newItem;
+        });
+        guessedCorrectly ? this.handleCorrectGuess(newData)
+        : this.handleIncorrectGuess(newData);
+    };
+
+    render() {
+        return (
+            <div>
+                <Nav score={this.state.score} topScore={this.state.topScore} />
+                <Header />
+                <Container>
+                    {this.state.characters.map(item => (
+                     <ClickItem
+                     key={item.id}
+                     id={item.id}
+                     shake={!this.state.score && this.state.topScore}
+                     handleClick={this.handleItemClick}
+                     image={process.env.PUBLIC_URL + item.image}
+                     />   
+                    ))}
+                </Container>
+            </div>
+        )
+    }
+}
+
+export default Game;
