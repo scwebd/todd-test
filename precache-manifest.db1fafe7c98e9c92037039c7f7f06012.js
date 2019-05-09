755ac3cf6067365428f9530074f0cf599fd87380
scwebd <scwebd@gmail.com>
2019-05-09 12:48:47 -0600

    Updates

---
+++ precache-manifest.db1fafe7c98e9c92037039c7f7f06012.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84253d7a89d4e4194f85272e1f00ce17",
+    "url": "/todd-test/index.html"
+  },
+  {
+    "revision": "3ee9c3c02169207f2b5b",
+    "url": "/todd-test/static/css/main.09bd050f.chunk.css"
+  },
+  {
+    "revision": "8b28b04b5b06edd2dc22",
+    "url": "/todd-test/static/js/2.dc948b19.chunk.js"
+  },
+  {
+    "revision": "3ee9c3c02169207f2b5b",
+    "url": "/todd-test/static/js/main.b303196e.chunk.js"
+  },
+  {
+    "revision": "e4cc76d15ed8b505b42b",
+    "url": "/todd-test/static/js/runtime~main.c2c8db26.js"
+  }
+]);
